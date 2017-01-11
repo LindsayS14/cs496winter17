e53822189b69e4d65ceade38b88927b19176cc2a
LindsayS14 <stollerl@oregonstate.edu>
2017-01-10 17:34:42 -0800

    Add files via upload
    update

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import time
-#http://stackoverflow.com/questions/3961581/in-python-how-to-display-current-time-in-readable-format
-time.ctime() #day month date time year
+from flask import Flask
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/')
+def telltime():
+	import time
+	#http://stackoverflow.com/questions/3961581/in-python-how-to-display-current-time-in-readable-format
+	return time.ctime() #day month date time year
+	
+@app.errorhandler(404)
+def page_not_found(e):
+	return 'Sorry, nothing is here.', 404
